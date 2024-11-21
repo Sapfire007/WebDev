1e60ecdaedceeffd15eca251270ec8fe5f6fb76a
Saptarshi Bose <165061481+Sapfire007@users.noreply.github.com>
2024-11-21 23:18:11 +0530

    Add files via upload

---
+++ Day60-Learning_about_Inserting_and_Removing_Elements_using_JavaScript/script.js
@@ -0,0 +1,19 @@
+console.log(document.querySelector(".box"));
+console.log(document.querySelector(".box").innerHTML);
+console.log(document.querySelector(".container").innerHTML);
+console.log(document.querySelector(".box").innerText);
+console.log(document.querySelector(".container").innerText);
+console.log(document.querySelector(".container").outerHTML);
+console.log(document.querySelector(".container").tagName);
+console.log(document.querySelector(".container").nodeName);
+console.log(document.querySelector(".container").textContent);
+console.log(document.querySelector(".container").hidden);
+// console.log(document.querySelector(".container").hidden = true);
+console.log(document.querySelector(".box").innerHTML = "I am Sap");
+console.log(document.querySelector(".box").hasAttribute("style"));
+console.log(document.querySelector(".box").getAttribute("style"));
+console.log(document.querySelector(".box").setAttribute("style", "display: inline"));
+console.log(document.querySelector(".box").attributes);
+console.log(document.querySelector(".box").removeAttribute("style"));
+console.log(document.querySelector(".box").dataset);
+
